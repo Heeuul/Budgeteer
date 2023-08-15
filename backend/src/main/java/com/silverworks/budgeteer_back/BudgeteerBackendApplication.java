2a4c1aaec67afb77c947590458249eb2a665e816
Hans <aimanhans@yahoo.com>
2023-08-15 10:03:04 +0800

    150823 - 10:02 - App Base Files

---
+++ backend/src/main/java/com/silverworks/budgeteer_back/BudgeteerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.silverworks.budgeteer_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgeteerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgeteerBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/silverworks/budgeteer_back/BudgeteerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silverworks.budgeteer_back;
+
+import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+class BudgeteerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
